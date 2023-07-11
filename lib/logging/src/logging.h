4733cbec51abf36cd7b19f912c3400b48d314ae7
Manuel Gonzalvez <manuelgonzalvez@gmail.com>
2023-07-11 15:45:58 -0300

    initial commit

---
+++ lib/logging/src/logging.h
@@ -0,0 +1,64 @@
+#ifndef mylogging_h
+#define mylogging_h
+
+#include "Arduino.h"
+#include <map>
+#include <mutex>
+
+
+// #define USE_MUTEX //Habilita el uso de un mutex previo a cada print. en implementaciones multithread puede ralentizar la ejecucion de todo!
+
+
+typedef enum
+{
+    LOGGER_NONE,   /*!< No log output */
+    LOGGER_ERROR,  /*!< Critical errors, software module can not recover on its own */
+    LOGGER_WARN,   /*!< Error conditions from which recovery measures have been taken */
+    LOGGER_INFO,   /*!< Information messages which describe normal flow of events */
+    LOGGER_DEBUG,  /*!< Extra information which is not necessary for normal use (values, pointers, sizes, etc). */
+    LOGGER_VERBOSE /*!< Bigger chunks of debugging information, or frequent messages which can potentially flood the output. */
+} loglevels;
+
+extern loglevels log_level;
+
+class logger
+{
+public:
+    // logger(uint8_t level);
+    logger(uint8_t level, char *tagName, bool add = false);
+    void setTagName(char *tagName);
+    // char *getTagName();
+    void setLevel(uint8_t level);
+    uint8_t getLevel();
+    bool printV();
+    bool printD();
+    bool printI();
+    bool printW();
+    bool printE();
+
+    friend class loggerList;
+    static std::mutex mtxl;
+
+private:
+    uint8_t _level;
+    char tagName[17];
+    bool printTag = true;
+};
+
+
+extern std::map<String, logger *> loggers;
+
+class loggerList
+{
+public:
+    static void addLogger(logger *l);
+    static logger *getLogger(String tagName);
+    static logger* getLoggerByIndex(int index);
+    static const char* getLoggerTagByIndex(int index);
+    static uint8_t getLoggerLevelByIndex(int index);
+    static int getLoggersCount();
+    static std::mutex mtx;
+
+};
+
+#endif mylogging_h
---
+++ src/logging.cpp
@@ -0,0 +1,182 @@
+#include "logging.h"
+#include <vector>
+
+loglevels log_level;
+std::map<String, logger *> loggers;
+std::mutex loggerList::mtx;
+std::mutex logger::mtxl;
+// logger::logger(uint8_t level)
+// {
+//     this->_level = level;
+//     // memcpy(this->tagName, "", sizeof(this->tagName));
+
+//     char tag[17];
+//     sprintf(tag, "logger_%d", (int)this);
+//     memcpy(this->tagName, tag, sizeof(this->tagName));
+//     // loggers.begin();
+//     // loggers.insert(std::make_pair(String(this->tagName), this));
+//     // loggerList::addLogger(this); // Add the logger to the loggers array
+// }
+
+logger::logger(uint8_t level, char *tag_, bool add)
+{
+    this->_level = level;
+    strncpy(this->tagName, tag_, sizeof(this->tagName));
+
+    if (add)
+        loggerList::addLogger(this);
+}
+
+void logger::setLevel(uint8_t level)
+{
+    this->_level = level;
+}
+
+uint8_t logger::getLevel()
+{
+    return this->_level;
+}
+
+void logger::setTagName(char *tagName)
+{
+    memcpy(this->tagName, tagName, sizeof(this->tagName));
+    Serial.printf("%s", this->tagName);
+}
+
+// char *logger::getTagName()
+// {
+//     return this->tagName;
+// }
+
+bool logger::printV()
+{
+#ifdef USE_MUTEX
+    std::lock_guard<std::mutex> lock(mtxl); // Lock the mutex
+#endif
+    if (this->_level >= LOGGER_VERBOSE)
+    {
+        Serial.flush(true);
+
+        Serial.printf("[%s]", this->tagName);
+    }
+
+    return (this->_level >= LOGGER_VERBOSE ? true : false);
+}
+bool logger::printD()
+{
+#ifdef USE_MUTEX
+    std::lock_guard<std::mutex> lock(mtxl); // Lock the mutex
+#endif
+    if (this->_level >= LOGGER_DEBUG)
+    {
+        Serial.flush(true);
+        Serial.printf("[%s]", this->tagName);
+    }
+    return (this->_level >= LOGGER_DEBUG ? true : false);
+}
+bool logger::printI()
+{
+#ifdef USE_MUTEX
+    std::lock_guard<std::mutex> lock(mtxl); // Lock the mutex
+#endif
+    if (this->_level >= LOGGER_INFO)
+    {
+        Serial.flush(true);
+        Serial.printf("[%s]", this->tagName);
+    }
+
+    return (this->_level >= LOGGER_INFO ? true : false);
+}
+bool logger::printW()
+{
+#ifdef USE_MUTEX
+    std::lock_guard<std::mutex> lock(mtxl); // Lock the mutex
+#endif
+    if (this->_level >= LOGGER_WARN)
+    {
+        Serial.flush(true);
+        Serial.printf("[%s]", this->tagName);
+    }
+    return (this->_level >= LOGGER_WARN ? true : false);
+}
+bool logger::printE()
+{
+#ifdef USE_MUTEX
+    std::lock_guard<std::mutex> lock(mtxl); // Lock the mutex
+#endif
+    if (this->_level >= LOGGER_ERROR)
+    {
+        Serial.flush(true);
+
+        Serial.printf("[%s]", this->tagName);
+    }
+    return (this->_level >= LOGGER_ERROR ? true : false);
+}
+
+void loggerList::addLogger(logger *l)
+{
+    std::lock_guard<std::mutex> lock(mtx); // Lock the mutex
+    loggers[String(l->tagName)] = l;
+}
+
+logger *loggerList::getLogger(String tagName)
+{
+    //   std::lock_guard<std::mutex> lock(mtx); // Lock the mutex
+
+    return loggers[tagName];
+}
+
+int loggerList::getLoggersCount()
+{
+    std::lock_guard<std::mutex> lock(mtx); // Lock the mutex
+
+    return loggers.size();
+}
+
+logger *loggerList::getLoggerByIndex(int index)
+{
+    std::lock_guard<std::mutex> lock(mtx); // Lock the mutex
+
+    if (index >= 0 && index < loggers.size())
+    {
+        auto it = loggers.begin();
+        std::advance(it, index);
+        return it->second;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+const char *loggerList::getLoggerTagByIndex(int index)
+{
+    std::lock_guard<std::mutex> lock(mtx); // Lock the mutex
+
+    if (index >= 0 && index < loggers.size())
+    {
+        auto it = loggers.begin();
+        std::advance(it, index);
+        return it->first.c_str();
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+uint8_t loggerList::getLoggerLevelByIndex(int index)
+{
+    std::lock_guard<std::mutex> lock(mtx); // Lock the mutex
+
+    if (index >= 0 && index < loggers.size())
+    {
+        auto it = loggers.begin();
+        std::advance(it, index);
+        return it->second->_level;
+    }
+    else
+    {
+        return 0;
+    }
+}
